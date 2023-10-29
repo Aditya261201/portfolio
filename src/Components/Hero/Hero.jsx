8ac1ce2e88ecb940fdadbece961140f91875f84a
adityaKumar <adityakumarrajput01@gmail.com>
2023-10-29 15:40:49 +0530

    hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Hero.css';
+import avatar from "./myAvatar.png";
+
+import { TypeAnimation } from 'react-type-animation';
+
+const Hero = () => {
+    return (
+        <section className='container'>
+            <div className='intro'>
+                <div className='textdiv'>
+                    <TypeAnimation
+                        preRenderFirstString={true}
+                        sequence={[
+                            500,
+                            'Hi, I\'m aditya',
+                            1000,
+                            'I\'m a web developer',
+                            1000,
+                            'I build things for the web',
+                            1000,
+                            'Exploring and learning new things....',
+                            500,
+                        ]}
+                        speed={50}
+                        style={{ fontSize: '4em' }}
+                        repeat={Infinity}
+                    />
+                </div>
+                <div className='btndiv'>
+                    <a href="mailto:myemail@gmail.com" className='contactbtn'>Contact me</a>
+                </div>
+            
+            </div>
+
+            <div className='avatar'>
+                <img src={avatar} height={350} width={300} alt='hero image' />
+            </div>
+        </section>
+    )
+}
+
+export default Hero
